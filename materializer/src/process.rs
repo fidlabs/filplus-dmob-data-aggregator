5e583c9477df43cbd8e1208628c2131e028df731
Kacper Żuk <kacper.zuk@neti-soft.com>
2024-08-01 16:49:42 +0200

    Add single retry for fetching stuff from replica

--- materializer/src/process.rs
+++ materializer/src/process.rs
@@ -1,8 +1,9 @@
 use crate::Result;
 use dest_db::{DestDatabase, Writable};
 use source_db::{Fetchable, SourceDatabase};
-use tracing::info;
+use tracing::{warn, info};
 use types::{AggregatedClientDeals, CidSharing, ProviderDistribution, ReplicaDistribution};
+use futures_util::future::TryFutureExt;
 
 #[tracing::instrument(skip(source_db, dest_db))]
 pub async fn process(source_db: SourceDatabase, dest_db: DestDatabase) -> Result<()> {
@@ -19,7 +20,19 @@ pub async fn process_view<T: Fetchable + Writable>(
     dest_db: &DestDatabase,
 ) -> Result<()> {
     info!("Fetching");
-    let data = source_db.fetch::<T>().await?;
+    let fetch = || source_db.fetch::<T>();
+    let data = fetch()
+        .or_else(|e| { 
+            // we're reading from a read-only postgres replica. long running
+            // queries can be cancelled if they're reading rows that primary db
+            // wants to remove/update. for now, lets just retry. if this keeps
+            // being a problem, increase max_standby_archive_delay and
+            // max_standby_streaming_delay in replica config
+            warn!(%e, "Error while fetching, retrying...");
+            fetch()
+        })
+        .await?;
+
 
     info!("Writing {} rows", data.len());
     dest_db
